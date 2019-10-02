878242e5e6b4345a5ad4f30b299d7b996e0b5712
Zach Nielsen <13890741+ZNielsen@users.noreply.github.com>
2019-10-02 11:23:22 -0700

    Changes to fail on warnings during CI check. (#16)
    * Changes to fail on warnings during CI check.
    
    * Added info about CI change to readme
    * Tell TravisCI just to build debug (faster build)

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@
 // license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
+#![cfg_attr(feature="fail-on-warnings", deny(warnings))]
 
-use std::{convert::TryInto, string::ToString, cmp::PartialEq};
+use std::{convert::TryInto, string::ToString};
 use structopt::StructOpt;
 
 #[derive(PartialEq)]
